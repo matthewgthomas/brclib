27d7786b5d48fa1c320beaf68fd411b2402c97d5
Matthew Gwynfryn Thomas <matthewgthomas@gmail.com>
2019-12-05 17:26:37 +0000

    Added get_month_num()

--- R/helpers.R
+++ R/helpers.R
@@ -27,3 +27,18 @@ convert_date = function(d) {
 #' @param x The proportion
 #' @export
 as_pc = function(x) round(x * 100, 1)
+
+#' Convert month name to the month's number
+#' (e.g. "February" or "Feb" returns 2)
+#'
+#' @param month String containing the month, either in full (e.g. "February") or abbreviated ("Feb")
+#' @param list.of.months Optional list of month names. Defaults to the full names (`month.name`); use `month.abb` for short month names
+#'
+#' @examples
+#' get_month_num("December")
+#' get_month_num("Dec", month.abb)
+#'
+#' @export
+get_month_num = function(month, list.of.months = month.name) {
+  match(month, list.of.months)
+}
